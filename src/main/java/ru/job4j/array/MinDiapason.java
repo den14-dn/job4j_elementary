bd91f68b59174b46b9154527efa1a562ad68cc64
den14-dn <den14-dn@yandex.ru>
2022-12-18 18:10:55 +0300

    6.5.2. Поиск минимума в диапазоне. [#179804 #372357]

--- src/main/java/ru/job4j/array/MinDiapason.java
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -3,8 +3,7 @@ package ru.job4j.array;
 public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
-        int bound = start + finish;
-        for (int i = start + 1; i < bound; i++) {
+        for (int i = start + 1; i <= finish; i++) {
             if (array[i] < min) {
                 min = array[i];
             }
