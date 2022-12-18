2638562e9e74f6a5cde75f4b175afee61d449595
den14-dn <den14-dn@yandex.ru>
2022-12-18 14:56:04 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        int bound = start + finish;
+        for (int i = start; i < bound; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+}
