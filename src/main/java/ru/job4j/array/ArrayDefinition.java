447ccade2186d80a86da56c239164e4d349eaf70
den14-dn <den14-dn@yandex.ru>
2022-12-18 13:34:09 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surname равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "John";
+        names[1] = "Michal";
+        names[2] = "Jan";
+        names[3] = "Nick";
+        System.out.println("Имя под индексом 0: " + names[0]);
+        System.out.println("Имя под индексом 1: " + names[1]);
+        System.out.println("Имя под индексом 2: " + names[2]);
+        System.out.println("Имя под индексом 3: " + names[3]);
     }
 }
