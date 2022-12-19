63e53606cd74d567d97a8ae6e33d7afe5a7f56f7
den14-dn <den14-dn@yandex.ru>
2022-12-19 08:49:49 +0300

    6.2.3. Перевернуть массив. [#226790 #372343]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,8 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int bound = array.length / 2;
-        for (int i = 0; i < bound; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - i - 1];
             array[array.length - i - 1] = temp;
