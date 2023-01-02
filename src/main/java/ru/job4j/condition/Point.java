319f03f8227da8a66532a01fd8bf137973e2c914
den14-dn <den14-dn@yandex.ru>
2023-01-02 23:41:23 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -3,12 +3,22 @@ package ru.job4j.condition;
 import static java.lang.Math.*;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point) {
+        return sqrt(pow(this.x - point.x, 2) + pow(this.y - point.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(1, 2, 2, 4);
-        System.out.println("result (1, 2) to (2, 4) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println("result (0, 0) to (0, 2) " + dist);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,44 +8,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(2, 0);
+        double out = p2.distance(p1);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when13to33then2() {
         double expected = 2;
-        int x1 = 1;
-        int y1 = 3;
-        int x2 = 3;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point p1 = new Point(1, 3);
+        Point p2 = new Point(3, 3);
+        double out = p2.distance(p1);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when13to23then1() {
         double expected = 1;
-        int x1 = 1;
-        int y1 = 3;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point p1 = new Point(1, 3);
+        Point p2 = new Point(2, 3);
+        double out = p2.distance(p1);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when13to47then5() {
         double expected = 5;
-        int x1 = 1;
-        int y1 = 3;
-        int x2 = 4;
-        int y2 = 7;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point p1 = new Point(1, 3);
+        Point p2 = new Point(4, 7);
+        double out = p2.distance(p1);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
